3d0e6979e3107a9cdb100230f2b8ed808a546053
tagraw <agrawal.tanvi101@gmail.com>
2025-08-21 21:21:17 -0500

    started main.py

--- model/model.py
+++ model/model.py
@@ -4,4 +4,9 @@ from sklearn.metrics import mean_squared_error
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 lr = LinearRegression().fit(X_train, y_train)
+preds = lr.predict(X_test)
+rmse = mean_squared_error(y_test, preds, squared=False)
+print("RMSE: ", rmse)
+
+
  
